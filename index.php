4ba790fcc5dc78a78decb857a8967efbb7b0f957
Janez Zibelnik <janez.zibelnik@agiledrop.com>
2022-05-17 15:27:58 +0200

    Initial commit.

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+
+
+
+
+$array = ['Apple', 'Orange', 'Lemon', 'Banana', 'Kiwi', 'Avocado', 'Elderberry'];
+
+$i = 1;
+foreach ($array as $value) {
+  echo 'Fruit nr.' . $i . ': ' . $value . '<br>';
+  $i = $i + 1;
+}
+echo '<br>';
+sort($array);
+$i = 1;
+
+foreach ($array as $value) {
+  if (in_array($value[0], ['A', 'E', 'I', 'O', 'U'])) {
+    echo 'Fruit nr.' . $i . ': ' . $value . '<br>';
+    $i = $i + 1;
+  }
+}
+
+$AssocArry = [
+  'key' => 1,
+  'key2' => 2,
+];
+
+arsort($AssocArry);
+$k = array_keys($AssocArry);
+var_dump($k);
+echo $k[0];
